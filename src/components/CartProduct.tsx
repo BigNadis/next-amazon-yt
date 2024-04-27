a4d7c9f9361052ca089c2279f945b175ef8a4494
BigNadis <133846614+BigNadis@users.noreply.github.com>
2024-04-27 15:45:24 +0400

    pushchanges

--- src/components/CartProduct.tsx
+++ src/components/CartProduct.tsx
@@ -23,10 +23,10 @@ interface cartProductsProps{
 }
 
 const CartProduct = ({item}: cartProductsProps) => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   return (
     <div className='bg-gray-100 rounded-lg flex items-center gap-4'>
-      <Image className='object-cover' width={150} height={150} src={item.image} alt="productImage" />
+      <Image className='object-cover w-full h-auto' width={150} height={150} src={item.image} alt="productImage" />
       <div className='flex items-center px-2 gap-4'>
         <div className='flex flex-col gap-1'>  
           <p className='text-lg font-semibold text-amazon_blue'>{item.title}</p>
@@ -40,23 +40,23 @@ const CartProduct = ({item}: cartProductsProps) => {
           <div className='flex items-center gap-6'>
             <div className='flex items-center mt-1 justify-between border border-gray-300 px-4 py-1 rounded-full w-28 shadow-lg shadow-gray-300'>
               <span
-              onClick={() =>
-                dispatch(
-                  increaseQuantity({
-                    _id: item._id,
-                    brand: item.brand,
-                    category: item.category,
-                    image: item.image,
-                    description: item.description,
-                    isNew: item.isNew,
-                    oldPrice: item.oldPrice,
-                    price: item.price,
-                    title: item.title,
-                    quantity: 1, 
-                  })
-                )
-              } 
-              className='w-6 h-6 flex items-center justify-center rounded-full text-base bg-transparent hover:bg-gray-300 cursor-pointer decoration-purple-300'>
+                onClick={() =>
+                  dispatch(
+                    increaseQuantity({
+                      _id: item._id,
+                      brand: item.brand,
+                      category: item.category,
+                      image: item.image,
+                      description: item.description,
+                      isNew: item.isNew,
+                      oldPrice: item.oldPrice,
+                      price: item.price,
+                      title: item.title,
+                      quantity: 1, 
+                    })
+                  )
+                } 
+                className='w-6 h-6 flex items-center justify-center rounded-full text-base bg-transparent hover:bg-gray-300 cursor-pointer decoration-purple-300'>
                 <LuPlus />
               </span>
               <span>{item.quantity}</span>
