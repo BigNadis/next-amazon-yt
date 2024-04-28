de3f298d6e9257e52db0caf2e19b7d53bab71f64
BigNadis <133846614+BigNadis@users.noreply.github.com>
2024-04-29 03:26:20 +0400

    push changes

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -9,7 +9,7 @@ import Link from 'next/link';
 const CartPage = () => {
   const { productData } = useSelector((state: StateProps) => state.next);
   return (
-    <div className='max-w-screen-2x1 mx-auto px-6 grid grid-cols-5 gap-10 py-4'>
+    <div className='max-w-screen-2x1 mx-auto px-6 grid grid-cols-2 gap-10 py-4'>
       {productData.length > 0 ? (
       <>
         <div className='bg-white col-span-4 p-4 rounded-lg'>
