8a1923eafee8ab7d3180e0eea1f3255b8bb78e23
BigNadis <133846614+BigNadis@users.noreply.github.com>
2024-04-29 14:44:21 +0400

    push changes

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -7,12 +7,12 @@ import CartPayment from '@/src/components/CartPayment';
 import Link from 'next/link';
 
 const CartPage = () => {
-  const { productData } = useSelector((state: StateProps) => state.next);
-  return (
-    <div className='max-w-screen-2x1 mx-auto px-6 grid grid-cols-5 gap-10 py-4'>
+ const { productData } = useSelector((state: StateProps) => state.next);
+ return (
+    <div className='max-w-screen-2x1 mx-auto px-6 grid grid-cols-1 sm:grid-cols-5 gap-10 py-4'>
       {productData.length > 0 ? (
       <>
-        <div className='bg-white col-span-4 p-4 rounded-lg'>
+        <div className='bg-white col-span-full sm:col-span-4 p-4 rounded-lg'>
             <div className='flex items-center justify-between border-b-[1px] border-b-gray-400 pb-1'>
               <p className='text-2x1 font-semibold text-amazon_blue'>
                 Shopping Cart
@@ -22,18 +22,18 @@ const CartPage = () => {
             <div className='pt-2 flex flex-col gap-2'>
               {productData.map((item: StoreProduct) => (
                 <div key={item._id} className='pt-2 flex flex-col gap-2'>
-                  <CartProduct item={item} />
+                 <CartProduct item={item} />
                 </div>
               ))}
               <ResetCart />
             </div>
           </div>
-          <div className='bg-white h-64 col-span-1 p-4 rounded-lg flex items-center justify-center'>
+          <div className='bg-white h-64 col-span-full sm:col-span-1 p-4 rounded-lg flex items-center justify-center'>
             <CartPayment />
           </div>
-      </>
+        </>
       ):(
-      <div className='bg-white h-64 col-span-5 flex flex-col items-center justify-center py-5 rounded-lg shadow-lg'>
+      <div className='bg-white h-64 col-span-full flex flex-col items-center justify-center py-5 rounded-lg shadow-lg'>
         <h1 className='text-lg font-medium'>Your cart is empty</h1>
         <Link href={"/"}>
           <button className='w-52 h-10 bg-amazon_blue text-white rounded-lg text-sm font-semibold hover:bg-amazon_yellow hover:text-black'>go to shopping</button>
@@ -41,7 +41,7 @@ const CartPage = () => {
       </div>
       )}
     </div>
-  );
+ );
 };
 
 export default CartPage;
