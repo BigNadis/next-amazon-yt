f6c61d8594470984f2c70e584980714240f9afb1
BigNadis <133846614+BigNadis@users.noreply.github.com>
2024-05-01 13:21:12 +0400

    push changes

--- src/components/header/BottomHeader.tsx
+++ src/components/header/BottomHeader.tsx
@@ -5,39 +5,42 @@ import { LuMenu } from "react-icons/lu";
 import { useDispatch, useSelector } from "react-redux";
 
 function BottomHead() {
-  const { userInfo } = useSelector((state: StateProps) => state.next);
-  const dispatch = useDispatch();
-  const handleSignOut = () => {
+ const { userInfo } = useSelector((state: StateProps) => state.next);
+ const dispatch = useDispatch();
+ const handleSignOut = () => {
     signOut();
     dispatch(removeUser());
-  }
-  return (
-    <div className="w-full h-10 bg-amazon_light text-sm text-white px-4 flex items-center">
-      <p className="flex items-center gap-1 h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300">
+ }
+
+ return (
+    <div className="w-full h-10 bg-amazon_light text-sm text-white px-4 flex flex-col sm:flex-row items-center justify-between">
+      <div className="flex items-center gap-1 h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300">
         <LuMenu className="text-xl" /> All
-      </p>
-      <p className="hidden md:inline-flex items-center h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300">
-        Today Deals
-      </p>
-      <p className="hidden md:inline-flex items-center h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300">
-        Customer Service
-      </p>
-      <p className="hidden md:inline-flex items-center h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300">
-        Registry
-      </p>
-      <p className="hidden md:inline-flex items-center h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300">
-        Gift Cards
-      </p>
-      <p className="hidden md:inline-flex items-center h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300">
-        Sell
-      </p>
+      </div>
+      <div className="flex flex-col sm:flex-row gap-4">
+        <p className="hidden sm:inline-flex items-center h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300">
+          Today Deals
+        </p>
+        <p className="hidden sm:inline-flex items-center h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300">
+          Customer Service
+        </p>
+        <p className="hidden sm:inline-flex items-center h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300">
+          Registry
+        </p>
+        <p className="hidden sm:inline-flex items-center h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300">
+          Gift Cards
+        </p>
+        <p className="hidden sm:inline-flex items-center h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300">
+          Sell
+        </p>
+      </div>
       {userInfo && (
-      <button onClick={handleSignOut} className="hidden md:inline-flex items-center h-8 px-2 border border-transparent hover:border-red-600 hover:text-red-400 text-amazon_yellow cursor-pointer duration-300">
-        Sign Out
-      </button>
-     )}
+        <button onClick={handleSignOut} className="hidden sm:inline-flex items-center h-8 px-2 border border-transparent hover:border-red-600 hover:text-red-400 text-amazon_yellow cursor-pointer duration-300">
+          Sign Out
+        </button>
+      )}
     </div>
-  );
-};
+ );
+}
 
 export default BottomHead;
